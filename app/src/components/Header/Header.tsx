40a6845a2d43b19af26c606ededf90f8439034e7
shiels <thebelfastflygon@gmail.com>
2022-12-23 11:29:24 +0100

    create menu on mobile

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -2,15 +2,29 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import logo from "../../shared/assets/logo.svg";
 import Text from "../../shared/constants/Text";
-import { Link } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { Drawer, Link, Stack, useMediaQuery } from "@mui/material";
+type Color = "GrayText" | "grey.900";
 
 const Header = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   const links = [
     Text.HeaderHome,
     Text.HeaderNew,
@@ -19,21 +33,63 @@ const Header = () => {
     Text.HeaderCategories,
   ];
 
-  const renderLinks = () => {
+  const renderLinks = (color: Color) => {
     return links.map((link, index) => {
-      return <Link color="GrayText" underline={"none"} sx={{px:2}}  key={index}> {link} </Link>;
+      return (
+        <Link color={color} underline={"none"} sx={{ px: 2 }} key={index}>
+          {link}
+        </Link>
+      );
     });
   };
 
+  const renderMenuMobile = () => {
+    return (
+      <React.Fragment>
+        <IconButton aria-label="open menu" onClick={handleDrawerOpen}>
+          <MenuIcon fontSize="large" sx={{ color: "grey.900" }} />
+        </IconButton>
+        <Drawer
+          elevation={0}
+          anchor={"right"}
+          open={open}
+          onClose={handleDrawerClose}
+        >
+          <Box sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+            <IconButton
+              sx={{ ml: "auto" }}
+              aria-label="close menu"
+              onClick={handleDrawerClose}
+            >
+              <CloseIcon fontSize="large" sx={{ color: "grey.900" }} />
+            </IconButton>
+            <Stack sx={{ mt: 6, pr: 10 }} spacing={2}>
+              {renderLinks("grey.900")}
+            </Stack>
+          </Box>
+        </Drawer>
+      </React.Fragment>
+    );
+  };
+
   return (
-    <Box sx={{ flexGrow: 1, mx:8, my:6 }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        mx: 8,
+        my: 6,
+        [theme.breakpoints.down("sm")]: {
+          mx: 4,
+          my: 3,
+        },
+      }}
+    >
       <AppBar elevation={0} color="transparent" position="static">
         <Toolbar>
           <Box sx={{ flexGrow: 1 }}>
             <img src={logo} alt="logo" />
           </Box>
-          <> {renderLinks()} </>
-
+          {matches ? renderMenuMobile() : <> {renderLinks("GrayText")} </>}
         </Toolbar>
       </AppBar>
     </Box>
